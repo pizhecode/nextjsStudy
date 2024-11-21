cb434a9955a83d46a76e852445b5be0b18e4094d
pizhe <pizhecode@outlook.com>
2024-11-22 01:45:38 +0800

    new

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Footer from "../../components/Footer";
+import Pizhe from '../../components/Pizhe'
+export default function Home() {
+  return (
+    <>
+    <div>3306</div>
+    <Pizhe title="皮喆"/>
+    <Footer/>
+    </>
+  );
+}
